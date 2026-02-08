5b007f9c63ad8f590456bc2e1f4eb6cc2bbff444
MzSterling <226895573+MzSterling@users.noreply.github.com>
2026-02-08 01:34:25 -0500

    modify file

--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
+# Health endpoint (for ALB)
+def health(request):
+    return HttpResponse("OK")
+
+
 def health(request):
     return HttpResponse("Media Analytics App is Running")
